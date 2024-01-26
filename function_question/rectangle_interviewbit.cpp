0b01db5220f48081564a7ef3534b21f0a85d6ba5
Rohit Vishwakarma <rohit8840@gmail.com>
2024-01-26 17:00:42 +0530

    no messages

---
+++ function_question/rectangle_interviewbit.cpp
@@ -0,0 +1,21 @@
+// Given four positive integers A, B, C, D, determine if there’s a rectangle such that the lengths of its sides are A, B, C and D (in any order).
+// If any such rectangle exist return 1 else return 0.
+
+/**
+ * @input A : Integer
+ * @input B : Integer
+ * @input C : Integer
+ * @input D : Integer
+ * 
+ * @Output Integer
+ */
+
+int Solve(int A,int B,int C,int D){
+
+if(A==B && C==D)
+return 1;
+else
+return 0;
+
+
+}
