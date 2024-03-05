ee320b3c0a614a2b2ec60f83488ab007ddd736ec
Rohit Vishwakarma <rohit8840@gmail.com>
2024-03-05 11:17:20 +0530

    adding

---
+++ spiral_leetcode.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int row=matrix.size(), col=matrix[0].size();
+    int top=0,bottom=row-1, left=col,right=col-1;
+
+  while(top<=bottom && left<=right){
+
+// print top
+for(int j=left;j<=right;j++)
+ans.push_back(matrix[top][j]);
+top++;
+
+// print right
+for(int i=top;i<=bottom;i++)
+ans.push_back(matrix[i][right]);
+right--;  
+
+//print bottom 
+if(top<=bottom){
+for(int i=right;i>=left;i--)
+ans.push_back(matrix[bottom][i]);
+bottom--;
+}
+
+// print left
+if(left<=right){
+for(int i=bottom;i>=top;i--)
+ans.push_back(matrix[i][left]);
+left++;
+}
+
+
+
+  }
+
+
+return ans;
+
+    }
+};
