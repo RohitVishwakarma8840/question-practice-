13943b9e0acfd2408bc6d71fe84f12b984e8a124
Rohit Vishwakarma <rohit8840@gmail.com>
2024-04-05 23:23:10 +0530

    code insertion

---
+++ Strings/Longest_Prefix_Suffix.cpp
@@ -0,0 +1,43 @@
+class Solution{
+
+public:		
+	int lps(string s) {
+	    // Your code goes here
+	    
+	    vector<int>lps(s.size(),0);
+	    int pre=0 , suf=1;
+	    
+	    
+	    while(suf<s.size()){
+	        
+	        // Match 
+	        if(s[pre]==s[suf]){
+	            
+	            lps[suf]=pre+1;
+	            pre++,suf++;
+	            
+	        }
+	        
+	        else{
+	            
+	            if(pre==0){
+	                lps[suf]=0;
+	                suf++;
+	               
+	            }
+	            
+	          else{
+	              
+	            pre=lps[pre-1];
+	             
+	          }  
+	          
+	        }
+	         
+	    }
+	    
+	    return lps[s.size()-1];
+	    
+	    
+	}
+};
