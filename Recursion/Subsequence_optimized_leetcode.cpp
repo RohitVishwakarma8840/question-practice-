bdad6f2a7c8b1c1f1d14a39da8dd1eb4626fd212
Rohit Vishwakarma <rohit8840@gmail.com>
2024-06-23 20:37:24 +0530

    code insertion

---
+++ Recursion/Subsequence_optimized_leetcode.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+
+void Subsequence(vector<int>&arr,int index, int size , vector<vector<int>>&ans,vector<int>&temp){
+
+if(index==size){
+    ans.push_back(temp);
+    return ;
+}
+
+// DO Not Add 
+Subsequence(arr,index+1,size,ans,temp);
+
+// Adding the 
+    temp.push_back(arr[index]);
+
+Subsequence(arr,index+1,size,ans,temp);
+
+temp.pop_back();
+
+
+}
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+vector<vector<int>>ans;
+vector<int>temp;
+
+Subsequence(nums,0,nums.size(),ans,temp);
+    return ans;
+
+    }
+    
+};
