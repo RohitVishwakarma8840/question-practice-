d7bd58a0d0e14eb4124afaa11064e616bad51194
Rohit Vishwakarma <rohit8840@gmail.com>
2024-07-04 13:39:22 +0530

    code insertion

---
+++ Recursion/Pow(X,N)_50_leetcode.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+double solve(double x , long n){
+
+if(n==0)
+return 1;
+
+if(n<0)
+return solve(1/x,-n);
+
+if(n%2==0)
+return solve(x*x,n/2);
+
+else
+return x*solve(x*x,(n-1)/2);
+
+
+}
+
+
+    double myPow(double x, long n) {
+
+        return solve(x,n);
+        
+    }
+};
